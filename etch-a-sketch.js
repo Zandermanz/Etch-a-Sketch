932c3fb874cb6fb644b9ea8b762d300a6cd0057b
Andrew Phillips <aphillips15@gmail.com>
2022-04-17 19:06:40 -0700

    set error check, encapsulated setGridSize

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,18 @@
-let gridSize = prompt("Choose your Grid Size")
-
-//Crudely sets max at 100
-if (gridSize > 100){gridSize =100};
-
-//Loop to create number of Div's imputed
-for(let i = 0; i < gridSize; i++){
-    addDiv();
+setGridSize(); //calls set grid size function
+function setGridSize(){
+    let gridSize = prompt("Choose your Grid Size");
+    //Prompts again if user enters more than 100
+    if (gridSize > 100){
+        gridSize = prompt("Please enter a grid size of 100 or less")
+    }
+    //Loop to create number of Div's imputed
+    for(let i = 0; i < gridSize; i++){
+        addDiv();
+    }
 }
+
+
+
 
 function addDiv(){
 const container = document.querySelector('#gridContainer');
